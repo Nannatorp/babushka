a4ba030f73be5216d3d190f38590335c63afccb8
Nanna Hansen <nann257j@stud.kea.dk>
2022-02-07 13:36:24 +0100

    first commit

---
+++ js.js
@@ -0,0 +1,54 @@
+const url = "https://babushka-dd8a.restdb.io/rest/menu";
+const options = {
+  headers: {
+    "x-apikey": "600ec2fb1346a1524ff12de4",
+  },
+};
+
+const section = document.querySelector("section");
+const template = document.querySelector("template").content;
+
+let filter = "alle";
+let data;
+const h1 = document.querySelector("h1");
+
+const filterKnapper = document.querySelectorAll("nav button");
+console.log(filterKnapper);
+filterKnapper.forEach((knap) => knap.addEventListener("click", filterMadden));
+
+function filterMadden() {
+  filter = this.dataset.kategori;
+
+  document.querySelector(".valgt").classList.remove("valgt");
+  this.classList.add("valgt");
+
+  vis(data);
+
+  h1.textContent = this.textContent;
+}
+
+async function hentData() {
+  const resspons = await fetch(url, options);
+  const json = await resspons.json();
+  data = json;
+  vis(data);
+}
+
+function vis(json) {
+  console.log(json);
+
+  section.textContent = "";
+
+  json.forEach((mad) => {
+    if (filter == mad.kategori || filter == "alle") {
+      const klon = template.cloneNode(true);
+      klon.querySelector("h2").textContent = mad.navn;
+      klon.querySelector(".info").textContent = mad.kortbeskrivelse;
+      klon.querySelector(".pris").textContent = "Pris: " + mad.pris + ",-";
+      klon.querySelector("img").src = "img/" + mad.billednavn + "-md.jpg";
+      section.appendChild(klon);
+    }
+  });
+}
+
+hentData();
